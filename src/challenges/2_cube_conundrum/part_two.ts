18d3fb3ea41268b026cd2e6cac8dd916b6a6ed2d
Nikola Krezeski <nkrezeski@banzae.dev>
2023-12-08 19:49:22 +0100

    feat: implement generation of challenge

---
+++ src/challenges/2_cube_conundrum/part_two.ts
@@ -0,0 +1,9 @@
+import { join } from 'path'
+import { readInput } from '../../utils'
+
+const $2_cube_conundrum_part_two = () => {
+  const input = readInput(join(__dirname, './input.txt'))
+  const inputLines = input.split('')
+}
+
+export default $2_cube_conundrum_part_two
---
+++ src/generate-challenge.ts
@@ -0,0 +1,31 @@
+import { mkdirSync, writeFileSync } from 'fs'
+import { join } from 'path'
+
+const getChallengeContent = (challengeName: string) =>
+  `import { join } from 'path'
+import { readInput } from '../../utils'
+
+const $${challengeName} = () => {
+  const input = readInput(join(__dirname, './input.txt'))
+  const inputLines = input.split('')
+}
+
+export default $${challengeName}
+`
+
+export const generateChallenge = (challengeName: string) => {
+  mkdirSync(join(__dirname, './challenges', challengeName))
+  writeFileSync(
+    join(__dirname, './challenges', challengeName, 'part_one.ts'),
+    getChallengeContent(`${challengeName}_part_one`),
+  )
+  writeFileSync(
+    join(__dirname, './challenges', challengeName, 'part_two.ts'),
+    getChallengeContent(`${challengeName}_part_two`),
+  )
+
+  writeFileSync(join(__dirname, './challenges', challengeName, 'input.txt'), '')
+}
+
+const [challengeName] = process.argv.slice(2)
+generateChallenge(challengeName)
